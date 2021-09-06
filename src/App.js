e945e7e4cfb87ee9d1a9b5f1fd831c96ec12dd02
Neenus Gabriel <hello@neenus.com>
2021-09-05 21:21:17 -0400

    fix(app.js): fix bug with scrolling to top on page refresh

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/HomePage";
 import AboutPage from "./pages/aboutpage/AboutPage";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    window.history.scrollRestoration = "manual";
+  }, []);
+
   return (
     <Router>
       <Switch>
